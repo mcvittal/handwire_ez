e5179f97a4144b5cca614766699f328feb1c6ebc
J.net <jennettereid@gmail.com>
2020-09-20 21:44:34 -0400

    add reset buttons in lieu of physical buttons

--- keyboards/handwired/dactyl_manuform/ez/keymaps/alex_ez/keymap.c
+++ keyboards/handwired/dactyl_manuform/ez/keymaps/alex_ez/keymap.c
@@ -18,9 +18,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB , KC_A  , KC_S   , KC_D   , KC_F  , KC_G  ,                       KC_H  , KC_J  , KC_K   , KC_L   ,KC_SCLN, KC_QUOT,
      KC_CAPS, KC_Z  , KC_X   , KC_C   , KC_V  , KC_B  ,                       KC_N  , KC_M  , KC_COMM, KC_DOT ,KC_SLSH, KC_NUBS,
      KC_LSFT, KC_LCTL,KC_LALT, KC_LGUI, KC_ENT,                                       KC_ENT, KC_RGUI, KC_RALT,KC_RCTL, KC_RSFT,
-                                               RAISE ,KC_SPC ,        KC_SPC, LOWER,
-                                                      KC_PGUP,        KC_HOME,
-                                       KC_PSCR,KC_BSLS,KC_PGDN,        KC_END ,KC_INS,KC_DEL 
+                                               RAISE ,KC_SPC ,        KC_SPC , LOWER,
+                                                      KC_HOME,        KC_END ,
+                                       KC_PSCR,KC_BSLS,RESET ,        RESET ,KC_INS,KC_DEL 
   ),
 
   [_LOWER] = LAYOUT_5x6(
